d4c5a8f8cbad35acaf854225fce0fcbf67734ebd
Vazyvite <jjprods@gmail.com>
2016-05-04 10:43:17 +0200

    déplacement nwjs
    Modification de la position du nwjs pour faciliter la distribution de
    l'application

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,3 @@
 "D:\Programmes\7-Zip\7z.exe" a -tzip pslgen.nw * -xr!*.nw -xr!*.eot -xr!*.ttf -xr!*.svg -xr!*.woff2 -xr!*.scss  -xr!node_modules
-.\node_modules\nwjs-sdk-v0.13.4-win-x64\nw.exe pslgen.nw
+..\nwjs-sdk-v0.13.4-win-x64\nw.exe pslgen.nw
 rem "C:\Program Files\nodejs\nwjs-v0.12.3-win-x64\nw.exe" pslgen.nw --remote-debugging-port=9221
